5c91ca72857606f51a888868f3bf08f67aa0952e
Valeria Vitvytska <leranmu@gmail.com>
2022-08-22 19:54:35 +0300

    update handleClick

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import './App.scss';
 import React, {Component} from 'react';
 import Modal from './Modal';
@@ -22,6 +21,17 @@ actions:<><button className='action_btn'>OK</button><button className='action_bt
 
 }
 
+handleClick =(index,className) =>{ this.setState((current)=>{ 
+    const newState ={...current}
+     newState.isOpenModal=true;
+     newState.text=modalDeclarations[index].text
+     newState.header=modalDeclarations[index].header
+     newState.backgroundColor = modalDeclarations[index].backgroundColor
+     newState.color= modalDeclarations[index].color
+     newState.actions=<><button className={className}>OK</button><button className={className}>Cancel</button></>
+    return newState
+})}
+
 render(){
 
 
@@ -36,9 +46,9 @@ return(
 
 
 
-<Button backgroundColor= {buttons[0].backGroundColor} handleClick ={() =>{ this.setState({isOpenModal:true,text:modalDeclarations[0].text,header:modalDeclarations[0].header,backgroundColor:modalDeclarations[0].backgroundColor,color:modalDeclarations[0].color,actions:<><button className='action_btn'>OK</button><button className='action_btn'>Cancel</button></>})}}>{buttons[0].text}</Button>
+<Button backgroundColor= {buttons[0].backGroundColor} handleClick ={()=>{this.handleClick(0,'action_btn')}}>{buttons[0].text}</Button>
     
-<Button backgroundColor= {buttons[1].backGroundColor}  handleClick ={() =>{ this.setState({isOpenModal:true,text:modalDeclarations[1].text,header:modalDeclarations[1].header,backgroundColor:modalDeclarations[1].backgroundColor,color:modalDeclarations[1].color,actions:<><button className='action_btn2'>OK</button><button className='action_btn2'>Cancel</button></>})}}>{buttons[1].text}</Button>
+<Button backgroundColor= {buttons[1].backGroundColor}  handleClick ={() =>{ this.handleClick(1,'action_btn2')}}>{buttons[1].text}</Button>
     
 </div>
 { isOpenModal &&
@@ -71,3 +81,4 @@ return(
 
 
 export default App;
+
