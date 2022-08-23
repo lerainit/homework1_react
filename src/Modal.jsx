c7df6068f17fc61ec05325109e1566c6852dda21
Valeria Vitvytska <leranmu@gmail.com>
2022-08-23 22:28:09 +0300

    Update Modal.jsx

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -7,7 +7,7 @@ import './App.scss';
 class Modal extends PureComponent{
   
     render(){
-  const {actions,text,header,handleClick,backgroundColor,color}  = this.props 
+  const {actions,text,header,handleClick,backgroundColor,color,closeButton}  = this.props 
 
 return(
 <>
@@ -15,7 +15,8 @@ return(
 <div className='modal' >
   <div className='modal_main_container' style={{backgroundColor,color}} text ={text} header={header} handleClick={handleClick} >
  
- <div className='header_container'><h2>{header}</h2><button className='close_btn' onClick={handleClick}>X</button></div> 
+ <div className='header_container'><h2>{header}</h2>
+ { closeButton && <button className='close_btn' onClick={handleClick}>X</button>}</div> 
 <p>{text}</p>
  {actions}
 </div>
